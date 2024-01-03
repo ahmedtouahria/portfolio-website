b2d348e75edaf5b5208a655a6e99ca8d005460e6
ahmedtouahria <ahmedtouahria2001@gmail.com>
2024-01-03 23:48:57 +0100

    debug settings

--- the_portfolio/settings.py
+++ the_portfolio/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG",True)
+DEBUG = False if config("DEBUG",False) == "False" else True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS",cast=Csv())
 
